15ff1c228dc45ec2d8656ed4266ea4b3865f32f9
supervueman <supervueman@gmail.com>
2021-04-21 13:18:28 +0400

    Add dpi sizes for canvas

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const WIDTH = 600;
 const HEIGHT = 200;
+const DPI_WIDTH = WIDTH * 2;
+const DPI_HEIGHT = HEIGHT * 2;
 
 function chart(canvas, data) {
   const ctx = canvas.getContext('2d');
 
   canvas.style.width = `${WIDTH}px`;
   canvas.style.height = `${HEIGHT}px`;
+  canvas.width = DPI_WIDTH;
+  canvas.height = DPI_HEIGHT;
 }
 
 chart(document.getElementById('chart'));
