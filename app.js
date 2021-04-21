03ffb1a5f6331dba2268e59cd9576dd6e7953d5f
supervueman <supervueman@gmail.com>
2021-04-21 13:15:35 +0400

    Set basic canvas sizes

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const WIDTH = 600;
 const HEIGHT = 200;
 
-function chart(canavs, data) {
+function chart(canvas, data) {
   const ctx = canvas.getContext('2d');
-  
 
+  canvas.style.width = `${WIDTH}px`;
+  canvas.style.height = `${HEIGHT}px`;
 }
 
 chart(document.getElementById('chart'));
